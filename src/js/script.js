acbaa5f9cd485dd174e628a21562da209511de53
Krossmanzs <radi.adi78@gmail.com>
2023-06-14 17:29:37 +0700

    Finish validation

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,40 @@ function validateEmail() {
     window.location.href = "./signup.html"
     return false
 }
+
+function validateForm(form) {
+    if(form.email.value == "") {
+        alert("Please provide your full name")
+        return false
+    }
+    
+    if (form.password.value == "") {
+        alert("Please provide your password")
+        return false
+    }
+    
+    if (form.passwordconfirm.value == "") {
+        alert("Please confirm your password")
+        return false
+    } else if (form.passwordconfirm.value != form.password.value) {
+        alert("Your confirmation password doesnt match with your password!")
+        return false
+    }
+
+    if (form.bod.value == "") {
+        alert("Please provide your birth of date!")
+        return false    
+    }
+
+    if (form.gender.value == "") {
+        alert("Please select your gender")
+        return false
+    }
+
+    if (!form.tos.checked) {
+        alert("You must agree our ToS!")
+        return false
+    }
+
+    confirm("Your request success, thank you for you effort!")
+}
